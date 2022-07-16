04b125966565785c2305ec8d50a91ef0821be622
unknown <thanhnampk1996@gmail.com>
2022-07-16 16:09:17 +0700

    update security

--- src/main/java/io/company/wbs/sercurity/SercurityConfig.java
+++ src/main/java/io/company/wbs/sercurity/SercurityConfig.java
@@ -42,7 +42,7 @@ public class SercurityConfig{
         // Get AuthenticationManager
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
